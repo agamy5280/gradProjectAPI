83a9e763b228aa76fcfde0f017afd10b622b1961
agamy5280 <marawan.agamy2010@hotmail.com>
2022-06-09 02:37:43 +0200

    Modifing gunicorn

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,11 +3,11 @@
 Gunicorn with Uvicorn config to launch in Digital Ocean's App Platform.
 """
 bind = "0.0.0.0:8080"
-workers = 10
+workers = 4
 client_header_timeout = 3000
 client_body_timeout = 3000
 fastcgi_read_timeout = 3000
 timeout = 3000
-threads = 120
+threads = 48
 # Uvicorn's Gunicorn worker class
 worker_class = "uvicorn.workers.UvicornWorker"
